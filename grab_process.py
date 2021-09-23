e6f08f6407a5c6a8d9153c0e82b0ba90a08e89a0
ingrid <ingridlove0812@gms.tku.edu.tw>
2021-09-23 23:17:13 +0800

    init for grab_process.py

---
+++ grab_process.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+import pandas as pd
+
+class grab():
+    def __init__(self, srt_date, end_date):
+        self.url = 'https://cn.investing.com/instruments/HistoricalDataAjax'
+        self.request_headers = {
+            'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36 Edg/93.0.961.47',
+            'content-type':'application/x-www-form-urlencoded',
+            'X-Requested-With':'XMLHttpRequest'
+        }
+        self.parameters = {
+            'curr_id':6408,
+            'smlID':1159963,
+            'header':'AAPL历史数据',
+            'st_date':srt_date,
+            'end_date':end_date,
+            'interval_sec':'Daily',
+            'sort_col':'date',
+            'sort_ord':'DESC',
+            'action':'historical_data',
+        }
+
+    def run(self):
+        r = requests.post(self.url, headers = self.request_headers, data = self.parameters)
+        soup = BeautifulSoup(r.text, "html.parser")
+        col = [c for c in re.sub(' ','\n',soup.find(['thead','th']).text).split('\n') if c != '']
+        dict1 = {key: [] for key in range(1,8)}
+        dict2 = {}
+        i = 1; l = 1
+        for listing in soup.find_all('tr'):
+            if l == len(soup.find_all('tr')):
+                for values in listing.find_all('td'):
+                    dict2[values.text.split(':')[0]] = values.text.split(':')[1]
+            else:
+                for values in listing.find_all('td'):
+                    dict1[i].append(values.text)
+                    i += 1
+                    if i % 7 == 1:
+                        i = 1
+            l += 1
+        table1 = pd.DataFrame.from_dict(dict1)
+        table1.columns = col
+        return table1.to_dict(), dict2
+
+# d1, d2 = grab('2021/09/13','2021/09/14').run()
